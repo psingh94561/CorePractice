e3fd18befc220ed8cc33f9183aefc76366f311a2
Pravendra Singh <Pravendra Singh@host.docker.internal>
2020-11-19 11:32:15 +0530

    Core java practice

---
+++ CoreJava_Practice/src/nestedClass/StaticLocalVar.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ *//*
+package com.nestedClass;
+
+*//**
+ * @author pravendra.singh
+ *
+ *//*
+
+
+class StaticLocalVar {
+public static void main(String args[]) { 
+	System.out.println(fun());
+}
+
+static int fun()
+{
+	static int x= 10; //Error: Static local variables are not allowed
+	return x--;
+}
+} */
